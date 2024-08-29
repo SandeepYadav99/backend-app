0e8298d4dc4caa906ed0d9c92fa36d0459729f58
SandeepYadav99 <sandeepyadav.7one@gmail.com>
2024-08-29 17:33:42 +0530

    [fix] Created Signup modals

--- controllers/SignupUser.js
+++ controllers/SignupUser.js
@@ -1,11 +1,16 @@
 import signupSchema from "../modals/SignupUser.js";
 import generatePasswordHash from "../utility/utility.js";
-const Signup = (req, res) => {
+const Signup = async(req, res) => {
   const { name, email, password } = req.body;
   const hashPassword = generatePasswordHash(password);
-  const user = signupSchema.create({ name, email, password: hashPassword });
+  const existingUser = await signupSchema.findOne({ email });
+ 
+  if (existingUser) {
+    return res.status(400).json({ msg: "Email already registered" });
+  }
+  const user =await signupSchema.create({ name, email, password: hashPassword });
 
-  res.status(201).json({ msg: "User created successfully", user });
+ return res.status(201).json({ msg: "User created successfully", user });
 };
 
 export default { Signup };
--- utility/utility.js
+++ utility/utility.js
@@ -3,7 +3,7 @@ import bcrypt from "bcrypt"
 const  generatePasswordHash=(password)=> {
     let salt = bcrypt.genSaltSync(10);
     let hash = bcrypt.hashSync(password, salt)
-    return hash;
+    return hash; 
 }
 
 export default generatePasswordHash;
