295c19bc310086dcf4808bc59cb6cd0958a29501
SandeepYadav99 <sandeepyadav.7one@gmail.com>
2024-08-29 18:10:52 +0530

    remove extra code

--- modals/SignupUser.js
+++ modals/SignupUser.js
@@ -1,15 +1,11 @@
 import mongoose from "mongoose";
-import jwt from 'jsonwebtoken'
+
 const signupSchema = new mongoose.Schema({
     name:String,
     email:String,
     password: String
 })
 
-signupSchema.methods.generateToken = function () {
-    var token = jwt.sign({ uid: this._id }, process.env.KEY);
-    return token;
-}
 const Signup = mongoose.model("users", signupSchema);
 
 export default Signup;
